59c00ac64a645daf05371fe9a853775ae11c3c42
CuizeYUE <johndoe@example.com>
2018-03-07 22:52:37 +0100

    Initial commit

---
+++ src/main/java/com/viseo/Controller/StudentController.java
@@ -0,0 +1,28 @@
+package com.viseo.Controller;
+
+import com.viseo.Entity.Student;
+import com.viseo.Service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public Collection<Student> getAllStudents(){
+        return studentService.getAllStudents();
+    }
+
+    @RequestMapping(value = "/{id}",method = RequestMethod.GET)
+    public Student getStudentById(@PathVariable("id") int id){
+        return studentService.getStudentById(id);
+    }
+}
